e4760d8a7e748ad90bdf64d8bb3879b8689b5567
samuel-ctrl <samsaras2002@gmail.com>
2023-09-21 13:41:42 +0530

    updated endpoinds

--- src/components/constent/endpoints.jsx
+++ src/components/constent/endpoints.jsx
@@ -1,7 +1,7 @@
 const APIENDPOINTS = {
-  USER_WAITLIST: "v1/user/",
-  USER_SUBSCRIBE: "v1/user/subscribe/",
-  USER_QUERY: "v1/user/query/",
+  USER_WAITLIST: "v1/user",
+  USER_SUBSCRIBE: "v1/user/subscribe",
+  USER_QUERY: "v1/user/query",
 };
 
 export default APIENDPOINTS;

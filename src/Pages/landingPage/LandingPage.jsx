2bd77ea3ca0cd0be26589075643e12de64b79e33
sharuk ahmed <sharukahmed321@gmail.com>
2023-11-23 17:07:19 +0530

    add static=true query param to test cloudflare cdn

--- src/Pages/landingPage/LandingPage.jsx
+++ src/Pages/landingPage/LandingPage.jsx
@@ -185,7 +185,8 @@ const LandingPage = ({ from, setFrom }) => {
           <img
             src={
               // DeviceScreenImage
-              "https://eaglebotlandingstorage.blob.core.windows.net/landing-static-files/device-screen.png?sp=r&st=2023-11-23T07:57:12Z&se=2023-11-23T15:57:12Z&spr=https&sv=2022-11-02&sr=b&sig=aE%2BEYcbhvG9Y9mnvLnEB9XSYcCMxsk8dnX8L3CowcmI%3D"
+              // "https://eaglebotlandingstorage.blob.core.windows.net/landing-static-files/device-screen.png?sp=r&st=2023-11-23T07:57:12Z&se=2023-11-23T15:57:12Z&spr=https&sv=2022-11-02&sr=b&sig=aE%2BEYcbhvG9Y9mnvLnEB9XSYcCMxsk8dnX8L3CowcmI%3D"
+              "https://eaglebotlandingstorage.blob.core.windows.net/landing-static-files/device-screen.png?static=true"
             }
             className={`${Style.device} ${Style.windowView}`}
             alt="eagle bot device image."
@@ -193,7 +194,8 @@ const LandingPage = ({ from, setFrom }) => {
           <img
             src={
               // DeviceScreenMobileImage
-              "https://eaglebotlandingstorage.blob.core.windows.net/landing-static-files/device-screen-mobile.png?sp=r&st=2023-11-23T07:59:43Z&se=2023-11-23T15:59:43Z&spr=https&sv=2022-11-02&sr=b&sig=utb9QG6P%2FW5Wu4fWs19s%2BggpI4nzqjNz9nVdHbNBPsc%3D"
+              // "https://eaglebotlandingstorage.blob.core.windows.net/landing-static-files/device-screen-mobile.png?sp=r&st=2023-11-23T07:59:43Z&se=2023-11-23T15:59:43Z&spr=https&sv=2022-11-02&sr=b&sig=utb9QG6P%2FW5Wu4fWs19s%2BggpI4nzqjNz9nVdHbNBPsc%3D"
+              "https://eaglebotlandingstorage.blob.core.windows.net/landing-static-files/device-screen-mobile.png?static=true"
             }
             className={`${Style.device} ${Style.mobileView}`}
             alt="eagle bot device image."
@@ -225,7 +227,8 @@ const LandingPage = ({ from, setFrom }) => {
                 poster={ApiAutomationThumbnail}
                 source={
                   // LaunchVideo
-                  "https://eaglebotlandingstorage.blob.core.windows.net/landing-static-files/LAUNCH%20VIDEO.mp4?sp=r&st=2023-11-15T13:16:44Z&se=2024-12-31T21:16:44Z&sv=2022-11-02&sr=b&sig=o4EEmZ%2FWf6XHI1KTOhKIu8C1ZnfJsKszmgqXKar4g9k%3D"
+                  // "https://eaglebotlandingstorage.blob.core.windows.net/landing-static-files/LAUNCH%20VIDEO.mp4?sp=r&st=2023-11-15T13:16:44Z&se=2024-12-31T21:16:44Z&sv=2022-11-02&sr=b&sig=o4EEmZ%2FWf6XHI1KTOhKIu8C1ZnfJsKszmgqXKar4g9k%3D"
+                  "https://eaglebotlandingstorage.blob.core.windows.net/landing-static-files/LAUNCH%20VIDEO.mp4?static=true"
                 }
               />
               <VideoComponent
@@ -233,7 +236,8 @@ const LandingPage = ({ from, setFrom }) => {
                 poster={BrowserThumbnail}
                 source={
                   // IntroVideo
-                  "https://eaglebotlandingstorage.blob.core.windows.net/landing-static-files/eagle_browser_and_chatbot_Intro.mp4?sp=r&st=2023-11-15T10:11:20Z&se=2025-01-01T18:11:20Z&sv=2022-11-02&sr=b&sig=UahBP9rzuwpgoj%2BAHhoP%2BXUyju9yk9frbl651pYgE%2B0%3D"
+                  // "https://eaglebotlandingstorage.blob.core.windows.net/landing-static-files/eagle_browser_and_chatbot_Intro.mp4?sp=r&st=2023-11-15T10:11:20Z&se=2025-01-01T18:11:20Z&sv=2022-11-02&sr=b&sig=UahBP9rzuwpgoj%2BAHhoP%2BXUyju9yk9frbl651pYgE%2B0%3D"
+                  "https://eaglebotlandingstorage.blob.core.windows.net/landing-static-files/eagle_browser_and_chatbot_Intro.mp4?static=true"
                 }
               />
               <VideoComponent
@@ -241,7 +245,8 @@ const LandingPage = ({ from, setFrom }) => {
                 poster={CanvasThumbnail}
                 source={
                   // CanvasV2
-                  "https://eaglebotlandingstorage.blob.core.windows.net/landing-static-files/Canvas_v2.mp4?sp=r&st=2023-11-15T10:05:49Z&se=2025-01-01T18:05:49Z&sv=2022-11-02&sr=b&sig=BgcHcW0THRfIaAZE9gOHpp27z%2BHll9xfipaXKORNcXQ%3D"
+                  // "https://eaglebotlandingstorage.blob.core.windows.net/landing-static-files/Canvas_v2.mp4?sp=r&st=2023-11-15T10:05:49Z&se=2025-01-01T18:05:49Z&sv=2022-11-02&sr=b&sig=BgcHcW0THRfIaAZE9gOHpp27z%2BHll9xfipaXKORNcXQ%3D"
+                  "https://eaglebotlandingstorage.blob.core.windows.net/landing-static-files/Canvas_v2.mp4?static=true"
                 }
               />
             </Slider>
@@ -367,7 +372,8 @@ const LandingPage = ({ from, setFrom }) => {
           <img
             src={
               // LaptopImage
-              "https://eaglebotlandingstorage.blob.core.windows.net/landing-static-files/golden-laptop.png?sp=r&st=2023-11-23T08:00:58Z&se=2023-11-23T16:00:58Z&spr=https&sv=2022-11-02&sr=b&sig=Qg9ZDVybRDvLhyA6e2ExeB%2BHc1iqpMYc82fShTzpuks%3D"
+              // "https://eaglebotlandingstorage.blob.core.windows.net/landing-static-files/golden-laptop.png?sp=r&st=2023-11-23T08:00:58Z&se=2023-11-23T16:00:58Z&spr=https&sv=2022-11-02&sr=b&sig=Qg9ZDVybRDvLhyA6e2ExeB%2BHc1iqpMYc82fShTzpuks%3D"
+              "https://eaglebotlandingstorage.blob.core.windows.net/landing-static-files/golden-laptop.png?static=true"
             }
             alt="Eagle Bot Golden Laptop"
           ></img>
